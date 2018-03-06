e6f9274d466106792f8e0df9fc397939ed644c62
ESKENAZI\waldir.bertuqui <wbn_k@hotmail.com>
2018-03-06 16:23:29 -0300

    add style in menu, change sidebarmenu and finished sidebar post area

---
+++ BlogFrontEnd/public/js/menuTopStyle.js
@@ -0,0 +1,60 @@
+/*mouse button*/
+$("div").scroll2Section({
+			  menu:'.scrollDownHome',
+			 offsetTop:90
+
+		});
+/*end*/
+
+
+
+
+
+
+
+
+/*menu style*/
+var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     if (scrollTop >= 250) {
+
+      /*downscale menu in top*/
+      $('.fa-bars').addClass('burgerSmall');
+      $('.fa-bars').removeClass('burger');
+      /*end*/
+
+      /*downcale menu logo in top*/
+      $('#logo').addClass('logoSmall');
+      $('#logo').removeClass('logo');
+      /*end*/
+
+      /*show menu in top*/
+      $(".menuTop").addClass('animated fadeInDown').show();
+      $('.menuTop').removeClass('fadeOut');
+      /*end*/
+
+    } else if (scrollTop < 250) {
+      
+      /*upnscale menu in top*/
+      $('.fa-bars').addClass('burger');
+      $('.fa-bars').removeClass('burgerSmall');
+      /*end*/
+
+      /*upscale menu in top*/
+      $('#logo').addClass('logo');
+      $('#logo').removeClass('logoSmall');
+      /*end*/
+
+      /*hide menu in top*/
+      $('.menuTop').removeClass('fadeInDown');
+      $('.menuTop').addClass('fadeOut');
+      /*end */
+
+
+      
+    } 
+    
+    
+  }); 
+ /*end*/
